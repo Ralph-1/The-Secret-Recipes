71b0f53baf4e5f352d06f9efd156b40db27dd9b0
Ralph <ralphoburu@gmail.com>
2021-10-05 16:22:57 +0300

    Add single Item

---
+++ src/containers/ItemDetails.js
@@ -0,0 +1,112 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import fetchMeal from '../actions/fetchSingle';
+import Loading from '../components/loading';
+import Image from '../components/RecipeImage';
+import RecipeDescription from '../components/Description';
+import { resetSelected } from '../actions/index';
+import {
+  getImg, getArea, getIntructions, getIngredient, getName, categoryName, getProductsPending,
+} from '../config/index';
+
+const MealDetails = ({
+  img,
+  area,
+  ingredients,
+  match,
+  fetchMeal,
+  name,
+  category,
+  pending,
+  resetSelected,
+ }) => {
+  const { id } = match.params
+
+  useEffect(() => {
+    fetchMeal(id);
+  }, [fetchMeal, id]);
+
+  const componentShouldRender = () => {
+    if (name === undefined || pending === true) return false;
+    return true;
+  };
+
+  if (!componentShouldRender()) {
+    return(<Loading />);
+  };
+
+  return (
+    <div>
+      <div>
+        <Image src={img} alt={name} />
+        <div>
+          <RecipeDescription category={category} area={area} ingredients={ingredients} />
+          <div>
+            <Link to="/">
+              <button type="button" onClick={resetSelected} className="btn">Home</button>
+            </Link>
+            <Link to={`/category/${category}`}>
+              <button type="button" onClick={resetSelected} className="btn">Back</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+ };
+
+ const mapStateToProps = state => {
+  const { singleMeal } = state;
+  const { details } = singleMeal;
+  return (
+    {
+      img: getImg(details),
+      area: getArea(details),
+      intructions: getIntructions(details),
+      ingredients: getIngredient(details),
+      name: getName(details),
+      category: categoryName(details),
+      pending: getProductsPending(details),
+    }
+  );
+};
+
+
+ const mapDispatchToProps = {
+  fetchMeal,
+  resetSelected,
+};
+
+ MealDetails.defaultProps = {
+  img: '',
+  pending: false,
+  name: '',
+  category: '',
+  area: '',
+  ingredients: [''],
+};
+
+ MealDetails.propTypes = {
+  img: PropTypes.string,
+  area: PropTypes.string,
+  pending: PropTypes.bool,
+  fetchMeal: PropTypes.func.isRequired,
+  ingredients: PropTypes.arrayOf(String),
+  match: PropTypes.shape({
+    path: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+  name: PropTypes.string,
+  category: PropTypes.string,
+  resetSelected: PropTypes.func.isRequired,
+};
+
+ export default connect(
+   mapStateToProps,
+   mapDispatchToProps,
+ )(MealDetails);
