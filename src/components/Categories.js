5373b1c04f8f09ffb0dd1016728aee1d4afe9f47
Ralph <ralphoburu@gmail.com>
2021-10-24 06:04:33 +0300

    Caetgoery styles

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Image from './RecipeImage';
+import '../assets/styles/categories.css';
 
 const Category = ({ src, name }) => (
-  <Image src={src} name={name} />
+  <Image src={src} name={name} className="category" />
 );
 
 Category.propTypes = {
--- src/components/CategoriesFilter.js
+++ src/components/CategoriesFilter.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../assets/styles/categoriesFilter.css';
 
 const CategoriesFilter = ({ onChange, value }) => {
   const categories = ['Beef', 'Chicken', 'Dessert', 'Lamb', 'Miscellaneous', 'Pasta', 'Pork', 'Seafood', 'Side', 'Starter', 'Vegan', 'Vegetarian', 'Breakfast', 'Goat'];
 
   return (
-    <div>
-      <h2>Pick Category</h2>
-      <select id="filter" onChange={onChange} value={value}>
+    <div className="flex m-3 cat-filter">
+      <h2>Select Category:</h2>
+      <select id="filter" onChange={onChange} value={value} className="filter">
         {categories.map((el) => (<option key={Math.random() * 1000} value={el}>{el}</option>))}
       </select>
     </div>
